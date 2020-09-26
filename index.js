65056dbad634262cb01fc723c3d027bb11d4b43c
sreech96 <67021839+sreech96@users.noreply.github.com>
2020-09-26 14:59:35 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+var randomNumber1 = Math.floor(Math.random() * 6) +1;
+
+var randomDiceImage = "Dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage2 = "Dice" + randomNumber2 + ".png";
+
+var randomImageSource2 = "images/" + randomDiceImage2;
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2)
+{
+  document.querySelector("h1").innerHTML = "😃Player1 wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "😃Player2 wins!";
+}
+else{
+  document.querySelector("h1").innerHTML = "Draw";
+}
